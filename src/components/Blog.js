9fde699e346f7e533ad1b58d4c0b25961138481b
Petri Kataja <peejkoo@gmail.com>
2019-12-25 20:01:20 +0200

    5.6 Added toggleable details for blog info

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,21 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false)
+  
+  const detailsOpen = {display: open ? '' : 'none'}
+  const toggleOpen = () => setOpen(!open)
+
+  return(
+    <div className='blog-entry'>
+      <div onClick={toggleOpen} className='title'> 
+        {blog.title} {blog.author}
+      </div>
+      <div style={detailsOpen} className='details'>
+        <a href={blog.url}>{blog.url}</a><br/>
+        {blog.likes} likes <button>like</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
