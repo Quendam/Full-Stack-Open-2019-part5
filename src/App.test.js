2677173636b14e11fa02a1f9edeb330bb16006c2
Petri Kataja <peejkoo@gmail.com>
2019-12-26 18:30:56 +0200

    5.17 Added test for logged in user

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,12 @@ import {
 jest.mock('./services/blogs')
 import App from './App'
 
+const user = {
+  username: 'taavi',
+  token: '1231231214',
+  name: 'Taavi Testaaja'
+}
+
 describe('<App />', () => {
   test('if no user logged, blogs are not rendered', async () => {
     const component = render(
@@ -19,4 +25,22 @@ describe('<App />', () => {
     const blogs = component.container.querySelector('.blog-entry')
     expect(blogs).toBeNull()
   })
+
+  test('blogs are rendered if user has logged in', async () => {
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+
+    const component = render(
+      <App />
+    )
+
+    component.rerender(<App />)
+
+    await waitForElement(
+      () => component.getByText('logout')
+    )
+
+    const blogs = component.container.querySelector('.blog-entry')
+    expect(blogs).not.toBeNull()
+  })
+
 })
